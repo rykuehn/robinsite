cf1cfdf250e91c0ccee639b4340132c6ce981e58
Robin Kuehn <kuehn.robin@gmail.com>
2017-04-10 23:33:51 -0700

    add scss styling to Header

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,37 +2,19 @@
 //var React = require('react');
 
 import React from 'react';
+import '../scss/components/_header.scss';
 
 const Header = () => {
   return ( 
-    <div style ={styles.container}>
-      <p style={styles.title}>About</p>
-      <p style={styles.title}> Contact </p>
-      <p style={styles.name}> Robin Kuehn </p>
-      <p style={styles.title}> Work </p>
-      <p style={styles.title}> Resume </p>
+    <div className="container">
+      <p className="title">About</p>
+      <p className="title"> Contact </p>
+      <p className="name"> Robin Kuehn </p>
+      <p className="title"> Work </p>
+      <p className="title"> Resume </p>
     </div> )
 }
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    height: '10vh',
-    fontFamily: 'Pavanam',
-    alignItems: 'center'
-  },
-  title: {
-    flexBasis: '50vmin',
-    textAlign: 'center',
-    fontSize: '18px'
-  },
-  name: {
-    flexBasis: '80vmin',
-    textAlign: 'center',
-    fontSize: '30px'
-  }
-} 
 
 export default Header;
 
